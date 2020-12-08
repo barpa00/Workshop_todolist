560bcfdf637d035756c819d5e73855778a4eb14e
barpa00 <barpamonster@gmail.com>
2020-12-08 13:33:28 +0800

    create

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,20 @@
 // TO DO
+document.addEventListener('DOMContentLoaded',function(){
+  document.querySelector('#addBtn').addEventListener('click',function(){
+    // console.log("oo")
+    let li =document.createElement('li')
+    // li.innerHTML='<span class="close">x</span>'
+    li.innerHTML= document.getElementById('input').value+'<span class="close">x</span>'
+    console.log( li.innerHTML)
+    // li.textContent = document.getElementById('input').value
+    // const oo = document.getElementById('input').value
+    // console.log(oo)
+    document.querySelector('ul').appendChild(li)
+    document.getElementById('input').value=''
+  })
+  // document.getElementsByClassName('close').addEventListener('click',function(){
+  //   alert("ppp")
+  //   console.log("close")
+  // })
+  // document.getElementsByClassName("close")
+})
