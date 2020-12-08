1855181bedae44caa737f7361cea4f3b81324aea
barpa00 <barpamonster@gmail.com>
2020-12-08 14:51:47 +0800

    deleted

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,20 +1,22 @@
 // TO DO
 document.addEventListener('DOMContentLoaded',function(){
+  //增加按鈕
   document.querySelector('#addBtn').addEventListener('click',function(){
-    // console.log("oo")
     let li =document.createElement('li')
-    // li.innerHTML='<span class="close">x</span>'
     li.innerHTML= document.getElementById('input').value+'<span class="close">x</span>'
-    console.log( li.innerHTML)
-    // li.textContent = document.getElementById('input').value
-    // const oo = document.getElementById('input').value
-    // console.log(oo)
+    // console.log( li.innerHTML)
     document.querySelector('ul').appendChild(li)
     document.getElementById('input').value=''
   })
-  // document.getElementsByClassName('close').addEventListener('click',function(){
-  //   alert("ppp")
-  //   console.log("close")
-  // })
-  // document.getElementsByClassName("close")
+  //deleted
+  var close = document.getElementsByClassName("close")
+  var i 
+  for(i=0;i<close.length;i++){
+    console.log(close[i])
+      close[i].onclick = function(){
+      var li = this.parentElement
+      li.remove()
+    }
+  }
+ 
 })
